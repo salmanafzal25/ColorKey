a498665658e099535de7a209e1394bec67e7f4a9
HelloDoctorDeveolopers <hellodoctordevelopers@gmail.com>
2020-07-03 16:11:49 +0500

    first

---
+++ Classes/Service.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  ColorKey
+//
+//  Created by HelloDoctor on 03/07/2020.
+//
+
+import Foundation
+
+
+public class Service{
+    
+    public static func getService() -> String{
+        return "this is salman service"
+    }
+    
+}
---
+++ Example/Pods/Target Support Files/ColorKey/ColorKey-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ColorKey : NSObject
+@end
+@implementation PodsDummy_ColorKey
+@end
